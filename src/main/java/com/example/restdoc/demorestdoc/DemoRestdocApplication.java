bec0d9e46291b2cfa8cbad15926bfbb30710033e
iMac <imac@iMacui-iMac.local>
2018-11-30 21:37:09 +0900

    restdoc

---
+++ src/main/java/com/example/restdoc/demorestdoc/DemoRestdocApplication.java
@@ -0,0 +1,12 @@
+package com.example.restdoc.demorestdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestdocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRestdocApplication.class, args);
+    }
+}
